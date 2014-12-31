30f36a1ca554b792872ca33be5dce88ad38bc6c6
Gulsah Kose <gulsah.1004@gmail.com>
2014-12-31 19:41:14 +0200

    Added debian packages

---
+++ deb_package_files/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pebble-remote):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="pebble-remote" command="/usr/bin/pebble-remote"
---
+++ deb_package_files/debian/pebble-remote.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pebble-remote initscript
+# sourced by /etc/init.d/pebble-remote
+# installed at /etc/default/pebble-remote by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
