080b39a9643a00a502184fe5eb2b40f9d5bf93d8
Gulsah Kose <gulsah.1004@gmail.com>
2014-12-31 20:11:37 +0200

    Deleted unnecessary files

--- deb_package_files/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pebble-remote):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pebble-remote" command="/usr/bin/pebble-remote"
--- deb_package_files/debian/pebble-remote.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pebble-remote package
-#
-0 4	* * *	root	[ -x /usr/bin/pebble-remote_maintenance ] && /usr/bin/pebble-remote_maintenance
