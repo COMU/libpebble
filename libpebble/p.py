792ba1183fcf69706116dc19053814d8f027815e
Gulsah Kose <gulsah.1004@gmail.com>
2015-01-11 14:45:18 +0200

    Corrected recovery problem. Fixes #15

--- libpebble/p.py
+++ libpebble/p.py
@@ -29,7 +29,10 @@ def cmd_remote_linux(pebble, args):
     def libreoffice_event_handler(event):
         right_click = "xdotool key --window "+ window_id + "Right"
         left_click = "xdotool key --window "+ window_id + "Left"
-        exit_click= "bash /usr/lib/python2.7/pebble/exit_click"
+        exit_click = "bash /usr/lib/python2.7/pebble/exit_click"
+        window_ids = pexpect.run("xdotool search --sync --onlyvisible --name \"libreoffice\"").split("\r\n") 
+        altf4_presentation = "xdotool windowactivate --sync "+window_ids[-2]+" key --clearmodifiers --delay 100 alt+F4"
+        altf4_edit = "xdotool windowactivate --sync "+window_ids[-3]+" key --clearmodifiers --delay 100 alt+F4"
 
         if event == "next":
             pexpect.run(right_click)
@@ -37,8 +40,14 @@ def cmd_remote_linux(pebble, args):
         if event == "previous":
             pexpect.run(left_click)
 
-	if event == "exit": 
-	    pexpect.run(exit_click)
+	if event == "exit":
+	    try:
+	        pexpect.run(altf4_presentation)
+	        pexpect.run(altf4_edit)
+	        pexpect.run(exit_click)
+            except Exception as e:
+                raise e
+        print event
 
     def music_control_handler(endpoint, resp):
         events = {
